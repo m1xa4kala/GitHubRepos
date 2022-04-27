a08e2885e1531862d3bd9cc8de76d7d32924e0bb
m1xa4kala <maikl2020011@gmail.com>
2022-04-27 11:56:40 +0300

    created component for repo item

---
+++ src/components/Repos/repoItem/RepoItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import github from '../../../assets/images/github.png'
+import star from '../../../assets/images/star.png'
+
+export default function RepoItem({ repo }) {
+  return (
+    <li className="repo">
+      <div className="repo__header">
+        <h3 className="repo__name">{repo.name}</h3>
+        <div className="repo__stars">
+          <img width={30} src={star} alt="star" />
+          {`  ${repo.stargazers_count}`}
+        </div>
+      </div>
+      <div className="repo__footer">
+        <div className="repo__last-update">
+          {`Updated At: ${new Date(repo.updated_at).toLocaleString()}`}
+        </div>
+        <a href={repo.html_url} className="repo__link">
+          <img width={50} src={github} alt="github" />
+        </a>
+      </div>
+    </li>
+  )
+}
