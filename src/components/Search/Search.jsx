4c54810c0d4c99ddf0c3968347b53f55329597d1
m1xa4kala <maikl2020011@gmail.com>
2022-04-27 11:07:08 +0300

    added styles for Search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import './search.scss'
 
 export default function Search() {
   return (
-    <div>Search</div>
+    <div className="search">
+      <input className="search__input" type="text" placeholder="Repo name" />
+      <button className="search__button btn">Search</button>
+    </div>
   )
 }
